57c9f7fb18bf5cfdb4a80fd094b68e5b8573fcc0
user.email <jon@relayr.io>
2016-09-30 10:28:23 +0200

    On settings click in includes the widget being clicked

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import  HumanReadableTimestamp from '../time/humanReadable';
 
-export default ({ title, lastMessage, showSettings, onSettingsClick }) => {
-    const settingsIcon = showSettings ? <i className="fa fa-cog mCWidgetIcon mQaSettings" onClick={onSettingsClick}></i> : '';
+export default ({ title, lastMessage, widget, showSettings, onSettingsClick }) => {
+    const settingsIcon = showSettings ? <i className="fa fa-cog mCWidgetIcon mQaSettings" onClick={() => onSettingsClick(widget)}></i> : '';
     return (
         <div className="rBoxHeader">
             {title}
