b026405e71916798bff6bfb31c3759a2e5f882ed
Lachlan McMillan <lachlan.mcmillan@relayr.io>
2017-01-16 11:27:56 +0100

    TP6492) Bugfix Number widget
    If number was too large, then the widget would overflow into neighbouring content.

--- src/number/index.js
+++ src/number/index.js
@@ -4,9 +4,19 @@ import LiveWidget from '../LiveWidget';
 import WidgetSizes from '../widgetSizes';
 
 function NumberWidget(props) {
+    const reading = props.state.reading;
+    const isNum = typeof reading == 'number';
+    const text = (!isNum ? reading : formatNumber(reading, 3))
+
     return (
-        <span className="mONumberWidget">{ props.state.reading }</span>
+        <span className="mONumberWidget">{ text }</span>
     );
+}
+
+function formatNumber(number, digitsAfterDecimal) {
+    // parsing back to float removes any zeros after the decimal place,
+    // ie. '1.200' is changed to '1.2' 
+    return parseFloat(number.toFixed(digitsAfterDecimal))
 }
 
 export default LiveWidget(NumberWidget, WidgetSizes.large);
